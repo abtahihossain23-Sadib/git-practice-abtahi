618b0984ee61a8addbfc64462f9ed5a1eadfdc36
git-practice-abtahi <abtahihossain23@gmail.com>
2026-06-15 11:32:31 +0600

    Add error handling for division

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from datetime import date
+from utils import add, subtract, multiply, divide
 
-print("Name: Abtahi Hossain")
+print("Abtahi Hossain")
 print("Today's Date:", date.today())
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
+print("Division:", divide(10, 0))
